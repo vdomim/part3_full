dbbfcd1904d734cd20c16c65cfa2a0c3c10daaae
Victor Dominguez <vdomim@gmail.com>
2023-07-25 15:01:18 +0200

    Uploading exxercise 3.17

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -102,6 +102,21 @@ app.post('/api/persons/', (req, res) => {
     })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true})
+    .then(updatedPerson => {
+        res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
