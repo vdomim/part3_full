f36c12fcee6434d88060bc789b0704596be6d5a9
Victor Dominguez <vdomim@gmail.com>
2023-07-25 02:01:11 +0200

    Uploading exercises 3.13

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const dotenv = require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -17,30 +19,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
     skip: function (req, res) { return req.method !== 'POST' }
   }))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-]
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
@@ -60,19 +39,16 @@ app.get('/info', (req, res) => {
 
 //Metodo para obtener todas las personas de la agenda
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    }) 
 })
 
 //Metodo para obtener una persona de la agenda
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person){
+    Person.findById(req.params.id).then(person => {
         res.json(person)
-    }else{
-        res.status(404).end()
-    }
+    })
 })
 
 //Metodo para eliminar una persona de la agenda
